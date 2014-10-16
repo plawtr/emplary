8ef98026699e767140954a988f57b125d887f24f
Andrey Krahmal <a.krahmal@plawtr.com>
2014-10-16 15:35:43 +0100

    devise migrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def index
 	end
@@ -23,6 +24,12 @@ class ApplicationController < ActionController::Base
     output = []
     error_messages.each {|k, v| output << "#{k.to_s.capitalize} #{v.join(", ")}."}
     output
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
   end
   
 end
--- db/migrate/20141016125642_create_users.rb
+++ db/migrate/20141016125642_create_users.rb
@@ -1,13 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.boolean :admin
-      t.string :email
-      t.string :first_name
-      t.string :last_name
+      t.boolean :admin,                  default: false
+      t.string :first_name,                                     null: false
+      t.string :last_name,                                      null: false
 
       t.timestamps
     end
-    add_index :users, :email, unique: true
+
   end
 end

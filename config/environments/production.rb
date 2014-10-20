b9ad6d9cfc78369aa4ca4aaaf980cf44a8e66b01
Andrey Krahmal <a.krahmal@plawtr.com>
2014-10-20 21:14:50 +0100

    serve assets from aws cloudfront in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Emplary::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "d3j0jrh6abkdhn.cloudfront.net"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.

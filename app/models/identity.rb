317025e79f72478d48fe893910c6c5522c163735
Andrey Krahmal <a.krahmal@plawtr.com>
2014-10-21 17:30:06 +0100

    asset precompile for asset sync

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,6 +1,8 @@
 class Identity < ActiveRecord::Base
   belongs_to :user
 
+  has_paper_trail
+
   validates_presence_of :uid, :provider
   validates_uniqueness_of :uid, :scope => :provider
 
--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module Emplary
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
     config.exceptions_app = self.routes
     # config.app_domain = "www.emplary.com"
